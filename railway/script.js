1b3006957e167410976c5d5d199a3024f7a5de60
malaclord <marton.borodi06@gmail.com>
2024-01-15 09:48:04 +0100

    added mobile support

--- railway/script.js
+++ railway/script.js
@@ -16,11 +16,42 @@ window.addEventListener("resize", (event) => {
 let canvasClickables = []
 
 canvas.addEventListener('mousemove', (event) => {
-	mouse.deltaX = event.clientX - mouse.x;
-	mouse.deltaY = event.clientY - mouse.y;
+	moveMouse(event.clientX,event.clientY)
+});
 
-	mouse.x = event.clientX;
-	mouse.y = event.clientY;
+let scaling = false;
+let touchDist = undefined;
+
+canvas.addEventListener('touchmove', (event) => {
+
+		event.preventDefault();
+	
+		if (scaling) {
+			const dist = Math.hypot(
+				event.touches[0].clientX - event.touches[1].clientX,
+				event.touches[0].clientY - event.touches[1].clientY);
+
+			if (touchDist) {
+				const deltaDist = touchDist-dist
+				zoom(deltaDist)
+			}; 
+
+
+			touchDist = dist;
+		} else moveMouse(event.touches[0].clientX,event.touches[0].clientY);
+
+
+	})
+
+let clickedStation = undefined;
+let clickedLine = undefined;
+
+function moveMouse(x,y) {
+	mouse.deltaX = x - mouse.x;
+	mouse.deltaY = y - mouse.y;
+
+	mouse.x = x;
+	mouse.y = y;
 
 	if (mouse.leftDown) {
 		view.x += mouse.deltaX / view.scale;
@@ -28,7 +59,7 @@ canvas.addEventListener('mousemove', (event) => {
 	}
 
 	render()
-});
+}
 
 function Clickable (x, y, x2, y2, clickEvent) {
 
@@ -46,29 +77,66 @@ function Clickable (x, y, x2, y2, clickEvent) {
 
 let clickStart = {x:0,y:0}
 
-canvas.addEventListener('mousedown', (event) => {
+function zoom(by) {
+	view.scale -= by * scrollSensitivity * view.scale;
+	render()
+}
+
+function startClick(x,y) {
 	mouse.leftDown = true;
+
+	mouse.x = x;
+	mouse.y = y;
+
 	clickStart.x = mouse.x;
 	clickStart.y = mouse.y;
-})
-canvas.addEventListener('mouseup', (event) => {
+}
+
+function endClick(click) {
 	mouse.leftDown = false;
 
 	clicked = false;
 
-	if (clickStart.x == mouse.x && clickStart.y == mouse.y) canvasClickables.forEach(c => {
+	if (clickStart.x == mouse.x && clickStart.y == mouse.y && click) canvasClickables.forEach(c => {
 		if (c.contains(mouse.x,mouse.y)) {
 			if (!clicked) c.clickEvent();
 			clicked = true;
 		}
 	});
+}
+
+canvas.addEventListener('mousedown', (event) => {
+	startClick(event.clientX,event.clientY);
+})
+canvas.addEventListener('mouseup', (event) => {
+	endClick(true);
 })
 
-const scrollSensitivity = .001;
+canvas.addEventListener("touchstart", (event) => {
+	if (event.touches.length === 2) {
+		scaling = true;
+	} else {
+		startClick(event.touches[0].clientX,event.touches[0].clientY);
+	}
+})
+
+canvas.addEventListener("touchend",(event) => {
+	endClick(true);
+
+	scaling = false;
+	touchDist = undefined;
+})
+
+canvas.addEventListener("touchcancel", (event) => {
+	endClick(false);
+
+	scaling = false;
+	touchDist = undefined;
+})
+
 
 canvas.addEventListener("wheel", (event) => {
-	view.scale -= event.deltaY * scrollSensitivity * view.scale;
-	render()
+	zoom(event.deltaY);
 })
 
 document.addEventListener('contextmenu', (event) => {
@@ -349,6 +417,11 @@ function renderStation(stat) {
 	}
 }
 
+window.mobileCheck = function() {
+	let check = false;
+	(function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+	return check;
+};
 
 function renderConnection(conn) {
 	const from = stations.filter(s=>{
@@ -382,7 +455,7 @@ function renderConnection(conn) {
 
 	ctx.strokeStyle = `#${color}`;
 
-	const clickLine = () => console.log(`Clicked on line "${lineName}"; ${from.label} -> ${to.label}.`);
+	const clickLine = () => {clickedLine = (`"${lineName}"; ${from.label} -> ${to.label}.`)};
 
 	line(x,y,x,
 		y2 + (y < y2 ? 4/view.scale : -4/view.scale)
@@ -403,6 +476,27 @@ function renderConnection(conn) {
 
 }
 
+const hideOptionsPanel = document.getElementById("hideOptionsPanel");
+const showOptionsPanel = document.getElementById("showOptionsPanel");
+
+hideOptionsPanel.addEventListener("click", (event)=>{
+	toggleOptionsPanel(false)
+})
+
+showOptionsPanel.addEventListener("click", (event)=>{
+	toggleOptionsPanel(true)
+})
+
+function toggleOptionsPanel(toggle) {
+	if (toggle) {
+		showOptionsPanel.style = "display: none"
+		optionsPanel.style = ""
+	} else {
+		showOptionsPanel.style = ""
+		optionsPanel.style = "display: none"
+	}
+}
+
 const debugInfoSpan = document.getElementById("debugInfo");
 const showNoConnectonsCheck = document.getElementById("showNoConnectionsCheck");
 const showClickBoxesCheck = document.getElementById("showClickBoxesCheck");
@@ -414,6 +508,12 @@ showClickBoxesCheck.addEventListener("click", (event) => {
 showNoConnectonsCheck.addEventListener("click",(event) => {
 	render();
 });
+
+const mobile = window.mobileCheck();
+const renderScale = mobile ? .5 : 1;
+
+const scrollSensitivity = mobile ? .0025 : .001;
+
 
 function render() {
 	canvas.height = window.innerHeight;
@@ -458,9 +558,12 @@ function render() {
 	- Lines drawn: ${linesDrawn};
 	- Circles drawn: ${circlesDrawn};
 	- Text drawn: ${textDrawn};
+	- View: ${view.x}, ${view.y}, ${view.scale};  
 	Data info:
 	- No. of stations: ${stations.length}/${allStations};
 	- No. of connections: ${connections.length};
+	- Station data: ${clickedStation};
+	- Line data: ${clickedLine};
 	`);
 
 
